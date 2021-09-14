63b5ad835e78ff1d0e6c16d39845267158ddabf8
cor <cor@pruijs.dev>
2021-09-14 18:36:09 +0200

    Cleanup

--- main.go
+++ main.go
@@ -1,43 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"strconv"
 )
 
 func main() {
-	//fmt.Println("Hello World")
 
-	// t = sequence number
-	// j = digit
+	for t := 1_234_567_889; t <= 9_999_999_999; t++ {
+		correct1 := checkDigit(0, 9, 0, 0, t)
+		correct2 := checkDigit(6, 4, 0, 1, t)
 
-	//var options []int
+		//correct3 := check(4, 0, 1, 1, t)
+		//correct4 := check(3, 6, 1, 2, t)
 
-	//fmt.Println(-6 % 10)
-	//
-	//encrypt("0613206176")
-	//
-	//for t := 1_234_567_889; t <= 9_999_999_999; t++ {
-	//	correct1 := checkDigit(0, 9, 0, 0, t)
-	//	correct2 := checkDigit(6, 4, 0, 1, t)
-	//
-	//	//correct3 := check(4, 0, 1, 1, t)
-	//	//correct4 := check(3, 6, 1, 2, t)
-	//
-	//	if correct1 && correct2 {
-	//		//options = append(options, t)
-	//		//fmt.Println(t)
-	//
-	//		if t == 1_234_567_890 {
-	//			fmt.Println("found")
-	//		}
-	//	}
-	//}
+		if correct1 && correct2 {
+			//options = append(options, t)
+			//fmt.Println(t)
 
-	//k := 1234
-	//for i := 0; i <= 9; i++ {
-	//	fmt.Printf("Unencrypted: %v encrypted: %v decrypted: %v\n", i, encryptDigit(i, k), decryptDigit(encryptDigit(i, k), k))
-	//}
+			//fmt.Println(t)
+			if t == 1_234_567_890 {
+				fmt.Println("found")
+			}
+		}
+	}
 }
 
 func digitKey(t int, i int, j int) int {
