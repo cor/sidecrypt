4f71d1ab882c718b9ae9a2ca140f234be53d0b0d
cor <cor@pruijs.dev>
2021-09-14 15:26:19 +0200

    Init empty go project

---
+++ main.go
@@ -0,0 +1,12 @@
+package main	// Package Declaration, every Go program must start with it
+				// White Spaces or Newline or Tabs, Go mostly doesn't care about this
+import "fmt"	// import is a keyword use to include code fro other package
+				// fmt is package 
+				// use of double quotes like this is known as String literal
+				
+/* ======Function Declration Starts==== */
+
+func main(){		// all functions start with keyword func followed by name of function
+	fmt.Println("Hello World")	//function inside fmt package called Println
+}
+
