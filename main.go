77eb3ae5712881345b8c11139ee3d0e61d95bbdb
cor <cor@pruijs.dev>
2021-09-14 15:57:27 +0200

    Check for one message

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
-package main	// Package Declaration, every Go program must start with it
-				// White Spaces or Newline or Tabs, Go mostly doesn't care about this
-import "fmt"	// import is a keyword use to include code fro other package
-				// fmt is package 
-				// use of double quotes like this is known as String literal
-				
-/* ======Function Declration Starts==== */
+package main
 
-func main(){		// all functions start with keyword func followed by name of function
-	fmt.Println("Hello World")	//function inside fmt package called Println
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	//fmt.Println("Hello World")
+
+	// t = sequence number
+	// j = digit
+
+	//correct1 := check(5, 0, 42, 0)
+	//correct2 := check(9, 6, 42, 0)
+
+	//fmt.Println(correct1)
+	//fmt.Println(correct2)
+
+	for t := 0; t < 1000000000; t++ {
+		correct1 := check(5, 0, t, 1)
+		correct2 := check(9, 6, t, 2)
+
+		//fmt.Println(getFloatDigit(math.Pi, t))
+
+		if correct1 && correct2 {
+
+			fmt.Println(t)
+		}
+		//fmt.Println(correct1)
+		//fmt.Println(correct2)
+	}
 }
 
+func check(encryptedDigit int, decryptedDigit int, t int, i int) bool {
+	s := math.Sin(float64(t + i))
+	k := getFloatDigit(s, i)
+
+	return ((encryptedDigit + k) % 10) == decryptedDigit
+}
+
+func getFloatDigit(number float64, digit int) int {
+	return (int)(math.Abs(number)*math.Pow10(digit)) % 10
+}
